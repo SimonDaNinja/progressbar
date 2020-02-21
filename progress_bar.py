bea561b7c1abf2782541c383cd8597253a814785
simonliljestrand <simon.liljestrand@smarteye.se>
2020-02-21 16:36:26 +0100

    added the progressbar

---
+++ progress_bar.py
@@ -0,0 +1,24 @@
+import time
+import random
+
+def PrintProgressBar(i,n,barlength=40, final = False):
+    iInt = int(i)
+    print(f"{(100*iInt)//n} %" + (4-len(str((100*iInt)//n)))*' ' +  " [" +\
+            "="*((barlength*iInt)//n) + (">" if ((barlength*iInt)//n)<barlength\
+            else "") + "."*(barlength-(barlength*iInt)//n-1) + "]",
+            end="\r",
+            flush=True)
+    if final:
+        print("Done!")
+
+if __name__ == "__main__":
+    d = 3
+    i = 0
+    tot = 5000
+
+    while i<tot:
+        i += d
+        i = min(i,tot)
+        d = max(min(d+random.uniform(-5,5),20),0)
+        PrintProgressBar(i,tot,10, i==tot)
+        time.sleep(.05)
