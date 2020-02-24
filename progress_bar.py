155804d96dc0337bd484e00d6e93126818933a9e
simonliljestrand <simon.liljestrand@smarteye.se>
2020-02-24 15:16:54 +0100

    added support for displaying extra information next to the progress bar

--- progress_bar.py
+++ progress_bar.py
@@ -1,4 +1,4 @@
-def PrintProgressBar(i,n,barlength=40, final = None):
+def PrintProgressBar(i,n,barlength=40, final = None, postMessage = ""):
     iInt = int(i)
     progressUnits = (barlength*iInt)//n
     # for portability, I avoid f-strings
@@ -10,7 +10,7 @@ def PrintProgressBar(i,n,barlength=40, final = None):
     barChars.insert(0,'[')
     barChars[barlength] = ']'
     barString = ''.join(barChars)
-    print(statusString + barString, end = "\r", flush=True)
+    print(statusString + barString + " " + postMessage, end = "\r", flush=True)
     if final is None:
         final = i>=n
     if final:
@@ -19,13 +19,19 @@ def PrintProgressBar(i,n,barlength=40, final = None):
 if __name__ == "__main__":
     import time
     import random
+    k = 0
     d = 3
+    dApprox = d
     i = 0
     tot = 5000
+
+
+    k += 1
 
     while i<tot:
         i += d
         i = min(i,tot)
         d = max(min(d+random.uniform(-5,5),20),0)
-        PrintProgressBar(i,tot, final = i==tot)
+        dApprox = dApprox*.9 + d*.1
+        PrintProgressBar(i,tot, final = i==tot, postMessage = "current progress speed: " + str(round(dApprox,3)))
         time.sleep(.05)
